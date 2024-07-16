cbe2cb0c717d22969382bc828974537384dba258
tatianacardosolima <tatidornel@gmail.com>
2024-07-16 11:53:36 -0300

    Adicionado Exemplo com gRPC

---
+++ src/Grpc.User.API/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+
+namespace Grpc.Users.API.Services
+{
+    public class UserService  : User.UserBase
+    {
+        private readonly ILogger<UserService> _logger;
+        public UserService(ILogger<UserService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<SaveUserResponse> Save(SaveUserRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new SaveUserResponse
+            {
+                Id = "1",
+                Message = "Usuário salvo com sucesso",
+                Success = true
+            });
+        }
+
+        public override Task<GetUserByIdResponse> GetById(GetUserByIdRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new GetUserByIdResponse
+            {
+                Email = "tatiana@lima.com",
+                Name = "tatiana lima"
+            });
+        }
+    }
+}
